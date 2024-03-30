57b0051a0850360b308055ecc266564b82a6653f
RuzGhandilian <159611923+RuzGhandilian@users.noreply.github.com>
2024-03-30 18:59:36 +0400

    added descriptions

--- model.py
+++ model.py
@@ -2,15 +2,59 @@ from imblearn.ensemble import BalancedRandomForestClassifier
 
 
 class Model:
+    """
+       A class representing a machine learning model based on Balanced Random Forest Classifier.
+
+       Attributes:
+       - clf (BalancedRandomForestClassifier): The Balanced Random Forest Classifier instance.
+
+       Methods:
+       - __init__: Constructor method for the Model class.
+       - fit: Method to fit the model to the training data.
+       - predict: Method to make predictions on new data.
+       """
+
     def __init__(self, n_estimators: int = 100, max_depth: int = None, min_samples_split: int = 2,
                  sampling_strategy: str = "all"):
+        """
+                Constructor method for the Model class.
+
+                Parameters:
+                - n_estimators (int): Number of trees in the forest.
+                - max_depth (int): Maximum depth of the tree.
+                - min_samples_split (int): Minimum number of samples required to split an internal node.
+                - sampling_strategy (str): Strategy used to resample the dataset.
+
+                Returns:
+                - None
+                """
+
         self.clf = BalancedRandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth,
                                                   min_samples_split=min_samples_split,
                                                   sampling_strategy=sampling_strategy)
 
     def fit(self, X, y):
+        """
+                Method to fit the model to the training data.
+
+                Parameters:
+                - X (array-like, shape (n_samples, n_features)): Training data.
+                - y (array-like, shape (n_samples,)): Target values.
+
+                Returns:
+                - None
+                """
         self.clf.fit(X, y)
 
     def predict(self, X):
+        """
+                Method to make predictions on new data.
+
+                Parameters:
+                - X (array-like, shape (n_samples, n_features)): New data to make predictions on.
+
+                Returns:
+                - y_proba (array-like, shape (n_samples,)): Predicted probabilities for the positive class.
+                """
         y_proba = self.clf.predict_proba(X)[:, 1]
         return y_proba
--- preprocessor.py
+++ preprocessor.py
@@ -4,13 +4,49 @@ from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 
 class Preprocessor:
+    """
+        A class representing a data preprocessor for dropping columns with much nan values, handling missing values and scaling features.
+
+        Attributes:
+        - nan_drop_threshold (float): Threshold for dropping columns with a high percentage of missing values.
+        - imputer: The imputer object for handling missing values.
+        - scaler: The scaler object for scaling features.
+        - cols_to_drop (list): List of column names to be dropped due to high percentage of missing values.
+
+        Methods:
+        - __init__: Constructor method for the Preprocessor class.
+        - fit: Method to fit the preprocessor on the training data.
+        - transform: Method to transform the input data using the fitted preprocessor.
+        - _get_imputer_by_name: Static method to get the imputer object based on the specified name.
+        - _get_scaler_by_name: Static method to get the scaler object based on the specified name.
+        """
     def __init__(self, nan_drop_threshold: float, imputing_strategy: str, scaling_strategy: str):
+        """
+               Constructor method for the Preprocessor class.
+
+               Parameters:
+               - nan_drop_threshold (float): Threshold for dropping columns with a high percentage of missing values.
+               - imputing_strategy (str): Strategy used for imputing missing values.
+               - scaling_strategy (str): Strategy used for scaling features.
+
+               Returns:
+               - None
+               """
         self.nan_drop_threshold = nan_drop_threshold
         self.imputer = self._get_imputer_by_name(imputing_strategy)
         self.scaler = self._get_scaler_by_name(scaling_strategy)
         self.cols_to_drop = None
 
     def fit(self, X):
+        """
+                Method to fit the preprocessor on the training data.
+
+                Parameters:
+                - X (DataFrame): Training data.
+
+                Returns:
+                - None
+                """
         count = X.isna().sum()
         self.cols_to_drop = list(X.columns[count / len(X) > self.nan_drop_threshold])
         X = X.drop(columns=self.cols_to_drop)
@@ -19,6 +55,15 @@ class Preprocessor:
         self.scaler.fit(X)
 
     def transform(self, X):
+        """
+                Method to transform the input data using the fitted preprocessor.
+
+                Parameters:
+                - X (DataFrame): Input data to be transformed.
+
+                Returns:
+                - X_transformed (DataFrame): Transformed data.
+                """
         X = X.drop(columns=self.cols_to_drop)
         X = self.imputer.transform(X)
         X = self.scaler.transform(X)
@@ -27,6 +72,15 @@ class Preprocessor:
 
     @staticmethod
     def _get_imputer_by_name(name: str):
+        """
+                Static method to get the imputer object based on the specified name.
+
+                Parameters:
+                - name (str): Name of the imputation strategy.
+
+                Returns:
+                - imputer: Imputer object.
+                """
         if name == "median":
             return SimpleImputer(strategy="median")
         elif name == "mean":
@@ -40,6 +94,15 @@ class Preprocessor:
 
     @staticmethod
     def _get_scaler_by_name(name: str):
+        """
+               Static method to get the scaler object based on the specified name.
+
+               Parameters:
+               - name (str): Name of the scaling strategy.
+
+               Returns:
+               - scaler: Scaler object.
+               """
         if name == "MinMaxScaler":
             return MinMaxScaler()
         elif name == "StandardScaler":
